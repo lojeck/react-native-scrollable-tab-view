bb3ede544a5f5eacd63cefbfed32da8a64c0790d
lojec <lojeck@yeah.net>
2019-08-22 11:27:34 +0800

    高度初始化问题 Platform.OS === 'ios' 替换为 true

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const ScrollableTabView = createReactClass({
     let positionAndroid;
     let offsetAndroid;
 
-    if (Platform.OS === 'ios') {
+    if (true) {
       scrollXIOS = new Animated.Value(this.props.initialPage * containerWidth);
       const containerWidthAnimatedValue = new Animated.Value(containerWidth);
       // Need to call __makeNative manually to avoid a native animated bug. See
@@ -118,7 +118,7 @@ const ScrollableTabView = createReactClass({
   },
 
   componentWillUnmount() {
-    if (Platform.OS === 'ios') {
+    if (true) {
       this.state.scrollXIOS.removeAllListeners();
     } else {
       this.state.positionAndroid.removeAllListeners();
@@ -127,7 +127,7 @@ const ScrollableTabView = createReactClass({
   },
 
   goToPage(pageNumber) {
-    if (Platform.OS === 'ios') {
+    if (true) {
       const offset = pageNumber * this.state.containerWidth;
       if (this.scrollView) {
         this.scrollView.getNode().scrollTo({x: offset, y: 0, animated: !this.props.scrollWithoutAnimation, });
@@ -216,7 +216,7 @@ const ScrollableTabView = createReactClass({
   },
 
   renderScrollableContent() {
-    if (Platform.OS === 'ios') {
+    if (true) {
       const scenes = this._composeScenes();
       return <Animated.ScrollView
         horizontal
@@ -313,7 +313,7 @@ const ScrollableTabView = createReactClass({
   },
 
   _onScroll(e) {
-    if (Platform.OS === 'ios') {
+    if (true) {
       const offsetX = e.nativeEvent.contentOffset.x;
       if (offsetX === 0 && !this.scrollOnMountCalled) {
         this.scrollOnMountCalled = true;
@@ -330,7 +330,7 @@ const ScrollableTabView = createReactClass({
     const { width, } = e.nativeEvent.layout;
 
     if (Math.round(width) !== Math.round(this.state.containerWidth)) {
-      if (Platform.OS === 'ios') {
+      if (true) {
         const containerWidthAnimatedValue = new Animated.Value(width);
         // Need to call __makeNative manually to avoid a native animated bug. See
         // https://github.com/facebook/react-native/pull/14435
